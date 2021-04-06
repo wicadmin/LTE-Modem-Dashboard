5bf1255d8e2c74c2a1fcd3f711b0799adb71e3bd
wicadmin <4563854+wicadmin@users.noreply.github.com>
2021-04-05 21:49:18 -0400

    Update getModemData.sh

--- getModemData.sh
+++ getModemData.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Let's kill any running ones just in case
-kill -9 $(pgrep -f getModemData.sh)
+# kill -9 $(pgrep -f getModemData.sh)
 
 
 while :; do
-   MM=$(mmcli -m 0 --command="!GSTATUS?")
+   MM=$(mmcli -m 3 --command="!GSTATUS?")
 
    RSSI_D=$(echo "${MM}" | grep "PCC RxD" | awk '{print $4}')
    RSRP_D=$(echo "${MM}" | grep "PCC RxD" | awk '{print $7}')
@@ -14,28 +14,20 @@ while :; do
    BAND=$(echo "${MM}" | grep "LTE band" | awk '{print $3}' | cut -c 2-)
    RSRQ=$(echo "${MM}" | grep "RSRQ" | awk '{print $3}')
    SINR=$(echo "${MM}" | grep "SINR" | awk '{print $3}' | sed 's/.$//')
-   DATE=$(date +%x' '%r)
 
 	
-sendStr = \
-$(jq -n \
---arg band "$BAND" \
---arg rssi_m "$RSSI_M" \
---arg rsrp_m "$RSRP_M" \
---arg rssi_d "$RSSI_D" \
---arg rsrp_d "$RSRP_D" \
---arg sinr "$SINR" \
---arg rsrq "$RSRQ" \
-'{band: $band, \
-rssi_m: $rssi_m,\
-rsrp_m: $rsrp_m, \
-rssi_d: $rssi_d, \
-rsrp_d: $rsrp_d, \
-sinr: sinr, \
-rsrq: $rsrq
-}')
+   sendStr=$(jq -n \
+	--argjson band "$BAND" \
+	--argjson rssi_m "$RSSI_M" \
+	--argjson rsrp_m "$RSRP_M" \
+	--argjson rssi_d "$RSSI_D" \
+	--argjson rsrp_d "$RSRP_D" \
+	--argjson sinr "$SINR" \
+	--argjson rsrq "$RSRQ" \
+	'{band: $band, rssi_m: $rssi_m,	rsrp_m: $rsrp_m, rssi_d: $rssi_d, rsrp_d: $rsrp_d, sinr: $sinr, rsrq: $rsrq}')
 
-mosquitto_pub -t 'modem/signal' -m '$sendStr'
+echo $sendStr
+mosquitto_pub -h <hostname> -t 'modem/signal' -m "$sendStr"
 
 sleep 5
 done
